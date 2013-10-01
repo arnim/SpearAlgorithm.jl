a9e5d9a624b94fd786e0e22096c15fbceb6d1ab3
Arnim Bleier <arnim.bleier@gmail.com>
2013-10-01 10:30:32 +0200

    fixing demo.jl

--- demo/demo.jl
+++ demo/demo.jl
@@ -9,11 +9,11 @@ A = generateAdjacencyMatrix(activities, C)
 users = readcsv("users.csv")
 resources = readcsv("resources.csv")
 
-iSortUsers = sortperm(vec(expertise), Sort.Reverse)
+iSortUsers = reverse(sortperm(vec(expertise)))
 sortedPrintU = reduce(*,map((i -> "$(users[i]) =>  $(round(expertise[i],3))\n"),iSortUsers))
 println("Users sorted by expertise \n$sortedPrintU")
 
-iSortResources = sortperm(vec(quality), Sort.Reverse)
+iSortResources = reverse(sortperm(vec(quality)))
 sortedPrintR = reduce(*,map((i -> "$(resources[i]) =>  $(round(quality[i],3))\n"),iSortResources))
 println("Resources sorted by quality \n$sortedPrintR")
 
